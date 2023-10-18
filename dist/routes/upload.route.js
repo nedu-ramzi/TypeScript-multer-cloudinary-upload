1c3b6d65dc626af16c6767d801ee1a2ef2dfafb8
Ramsey Chinedu <ramseychinedu@gmail.com>
2023-10-18 09:58:51 +0100

    Updated package.json

--- dist/routes/upload.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const multer_middleware_1 = require("../middleware/multer.middleware");
-exports.router = (0, express_1.Router)();
-exports.router.post('/upload', multer_middleware_1.handler);
-//# sourceMappingURL=upload.route.js.map
--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ import { config } from './config/main.config';
 import { appRouter } from './routes/upload.route';
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 app.get('/', (req, res) => res.send('App Successfully Running'));
 
 //route
-app.use('/api/v1', appRouter)
+app.use('/api/v1', appRouter);
 server.listen(config.server.port, () => console.log(`Application Running on port ${config.server.port}`));
